d0d754741cdfe7ea6ad6481ae293041594a58218
Tomoki　Mori <TomokiMori1974@users.noreply.github.com>
2013-12-31 00:15:39 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,22 +18,22 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '100000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '101000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '102000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '103000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '200000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '201000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '202000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '203000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '300000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '301000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '302000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '303000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '400000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '401000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '900000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
-  '901000': { icon: 'icons/civilian-action.svg', color: '#30276f'}
+  '100000': { icon: 'icons/planning.svg', color: '#30276f'},
+  '101000': { icon: 'icons/human-resources.svg', color: '#30276f'},
+  '102000': { icon: 'icons/family.svg', color: '#30276f'},
+  '103000': { icon: 'icons/public-dept.svg', color: '#30276f'},
+  '200000': { icon: 'icons/economy-tourism.svg', color: '#30276f'},
+  '201000': { icon: 'icons/car.svg', color: '#30276f'},
+  '202000': { icon: 'icons/our-streets.svg', color: '#30276f'},
+  '203000': { icon: 'icons/economic-aid.svg', color: '#30276f'},
+  '300000': { icon: 'icons/publicaffairs.svg', color: '#30276f'},
+  '301000': { icon: 'icons/money.svg', color: '#30276f'},
+  '302000': { icon: 'icons/pound.svg', color: '#30276f'},
+  '303000': { icon: 'icons/dollar.svg', color: '#30276f'},
+  '400000': { icon: 'icons/financial-admin.svg', color: '#30276f'},
+  '401000': { icon: 'icons/financial-admin.svg', color: '#30276f'},
+  '900000': { icon: 'icons/unknown.svg', color: '#30276f'},
+  '901000': { icon: 'icons/unknown.svg', color: '#30276f'}
 
 
 };
