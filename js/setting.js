44f2ff80353c48ee0478f3edb82a0bc89922283e
Tomoki　Mori <TomokiMori1974@users.noreply.github.com>
2013-12-30 23:24:05 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'iga_2013_budget';
+OpenSpending.identifier = 'iga_2013_budget2';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
@@ -18,55 +18,23 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
-  '101': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#0AB971' },
-  '201': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#0AB971' },
-  '202': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#0AB971' },
-  '203': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#0AB971' },
-  '204': { icon: 'icons/vote.svg', color: '#C75746', bcolor: '#0AB971' },
-  '205': { icon: 'icons/ekonomija.svg', color: '#C75746', bcolor: '#0AB971' },
-  '206': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#0AB971' },
-   '3': { icon: 'icons/family2.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '301': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '302': { icon: 'icons/old-age.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '303': { icon: 'icons/courts.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '304': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#D33673' },
-  '305': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#D33673' },
-  '306': { icon: 'icons/bousai.svg', color: '#C75746', bcolor: '#D33673' },
- '4': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#D33673' },
- '401': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#D33673' },
-  '402': { icon: 'icons/waste.svg', color: '#C75746', bcolor: '#D33673' },
-  '5': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#D33673' },
-  '501': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '6': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#EC2406' },
-  '601': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#EC2406' },
-  '602': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#EC2406' },
-  '7': { icon: 'icons/syoukougyou.svg', color: '#C75746', bcolor: '#938626' },
-  '701': { icon: 'icons/syoukougyou.svg', color: '#C75746', bcolor: '#938626' },
-  '8': { icon: 'icons/construction.svg', color: '#C75746', bcolor: '#C75746' },
-  '801': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#C75746' },
-  '802': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#C75746' },
-  '803': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#C75746' },
-  '804': { icon: 'icons/planning.svg', color: '#C75746', bcolor: '#C75746' },
-  '805': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#C75746' },
-  '9': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#D33673' },
-  '901': { icon: 'icons/bousai.svg', color: '#C75746', bcolor: '#D33673' },
-  '10': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' },
-  '1001': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#790586' },
-  '1002': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' },
-  '1003': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' },
-  '1004': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' },
-  '1005': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#790586' },
-  '1006': { icon: 'icons/sports.svg', color: '#C75746', bcolor: '#790586' },
-  '11': { icon: 'icons/bousai.svg', color: '#C75746', bcolor: '#790586' },
- '1101': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#790586' },
- '1102': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#790586' },
- '1103': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#790586' },
- '12': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#790586' },
- '1201': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' },
- '13': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' },
- '1301': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' }
+  '100000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '101000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '102000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '103000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '200000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '201000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '202000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '203000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '300000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '301000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '302000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '303000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '400000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '401000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '900000': { icon: 'icons/civilian-action.svg', color: '#30276f'},
+  '901000': { icon: 'icons/civilian-action.svg', color: '#30276f'}
+
 
 };
 
